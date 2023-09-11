f9ac297c1ede347383ed034d04103d9647a205c4
Paul Nguon <herropaul@gmail.com>
2023-09-11 11:54:54 -0700

    updated home page design and working on blobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,40 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
-      <div className="space-y-4 space-x-4">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Tournaments
-        </button>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Team Rankings
-        </button>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Global Rankings
-        </button>
+    <div className="flex flex-col">
+      <nav className="flex space-x-4 mb-4 w-full items-start">
+        <button className="font-bold py-2 px-4 rounded">Tournaments</button>
+        <button className="font-bold py-2 px-4 rounded">Team Rankings</button>
+        <button className="font-bold py-2 px-4 rounded">Global Rankings</button>
+      </nav>
+      <div className="flex flex-col items-center justify-center min-h-screen relative w-full">
+        <div className="m-auto">
+          <h1 className=" text-8xl font-bold mb-4 text-center">
+            Global Power Ranking
+          </h1>
+          <h2 className="text-3xl font-semibold mb-2 text-center">
+            Predictions for LoL esports teams
+          </h2>
+          <div className="flex justify-center items-center">
+            <div
+              className="w-48 h-48 bg-yellow-500 blur-xl"
+              style={{
+                borderRadius: "28% 72% 79% 21% / 21% 40% 60% 79% ",
+              }}
+            ></div>
+            <div
+              className="w-48 h-48 bg-red-500 rounded-full -ml-10 blur-xl"
+              style={{
+                borderRadius: "61% 39% 19% 81% / 72% 40% 60% 28% ",
+              }}
+            ></div>
+            <div
+              className="w-48 h-48 bg-blue-500 rounded-full -ml-10 blur-xl"
+              style={{
+                borderRadius: "61% 39% 19% 81% / 29% 88% 12% 71% ",
+              }}
+            ></div>
+          </div>
+        </div>
       </div>
     </div>
   );
