4e1aa41994d03f5e877e5d2b4d37a5cf5826744b
Paul Nguon <herropaul@gmail.com>
2023-09-13 18:36:47 -0700

    animated blob moves in circular motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { AnimatedBlob } from "./components/AnimatedBlob";
+
 export default function Home() {
   return (
     <div className="flex flex-col">
@@ -7,44 +10,38 @@ export default function Home() {
         <button className="font-bold py-2 px-4 rounded">Global Rankings</button>
       </nav>
       <div className="flex flex-col items-center justify-center min-h-screen relative w-full">
-        <div className="m-auto">
+        <AnimatedBlob />
+        <div className="">
           <h1 className=" text-8xl font-bold mb-4 text-center">
             Global Power Ranking
           </h1>
           <h2 className="text-3xl font-semibold mb-2 text-center">
             Predictions for LoL esports teams
           </h2>
-          <div className="flex justify-center items-center">
-            <div
-              className="w-48 h-48 blur-xl"
-              style={{
-                borderRadius: "28% 72% 79% 21% / 21% 40% 60% 79% ",
-                backgroundColor: "#B99D76",
-              }}
-            ></div>
-            <div
-              className="w-48 h-48 rounded-full -ml-10 blur-xl"
-              style={{
-                borderRadius: "61% 39% 19% 81% / 72% 40% 60% 28% ",
-                backgroundColor: "#0080DA",
-              }}
-            ></div>
-            <div
-              className="w-48 h-48 rounded-full -ml-10 blur-xl"
-              style={{
-                borderRadius: "61% 39% 19% 81% / 29% 88% 12% 71% ",
-                backgroundColor: "#00218A",
-              }}
-            ></div>
-            <div
-              className="w-48 h-48 rounded-full -ml-10 blur-xl"
-              style={{
-                borderRadius: "29% 71% 19% 81% / 47% 20% 80% 53% ",
-                backgroundColor: "#7391AF",
-              }}
-            ></div>
-          </div>
         </div>
+        {/* <div className="flex justify-center items-center">
+          <div
+            className="w-48 h-48 rounded-full -ml-10 blur-xl"
+            style={{
+              borderRadius: "61% 39% 19% 81% / 72% 40% 60% 28% ",
+              backgroundColor: "#0080DA",
+            }}
+          ></div>
+          <div
+            className="w-48 h-48 rounded-full -ml-10 blur-xl"
+            style={{
+              borderRadius: "61% 39% 19% 81% / 29% 88% 12% 71% ",
+              backgroundColor: "#00218A",
+            }}
+          ></div>
+          <div
+            className="w-48 h-48 rounded-full -ml-10 blur-xl"
+            style={{
+              borderRadius: "29% 71% 19% 81% / 47% 20% 80% 53% ",
+              backgroundColor: "#7391AF",
+            }}
+          ></div>
+        </div> */}
       </div>
     </div>
   );
