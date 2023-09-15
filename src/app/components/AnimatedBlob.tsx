965a00727108cec169d6c5fdc2c6d6fec2c079be
Paul Nguon <herropaul@gmail.com>
2023-09-15 16:03:48 -0700

    added aws amplify and blobs have been animated

--- src/app/components/AnimatedBlob.tsx
+++ src/app/components/AnimatedBlob.tsx
@@ -1,46 +1,43 @@
 import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
 
-// const blobVariants = {
-//   rotate: { rotate: 360 },
-//   vertical: { y: ["0%", "100%", "0%"] },
-//   horizontal: { x: ["0%", "100%", "0%"] },
-// };
-const blobVariants = {
-  around: {
-    // This animation moves the blob in a square path.
-    // The x and y values are defined as percentages of the container's width and height.
-    // The blob starts at the top-left corner (0%, 0%) and moves to the top-right corner (100%, 0%),
-    // then to the bottom-right corner (100%, 100%), then to the bottom-left corner (0%, 100%),
-    // and finally back to the top-left corner (0%, 0%).
-    x: ["0%", "100%", "100%", "0%", "0%"],
-    y: ["0%", "0%", "100%", "100%", "0%"],
-  },
-};
+interface AnimatedBlobType {
+  radius: number;
+  duration: number;
+  top: number;
+  left: number;
+  borderRadius: string;
+  backgroundColor: string;
+  clockwise: boolean;
+}
 
-const transition = {
-  x: { duration: 15, repeat: Infinity, repeatType: "loop", ease: "linear" },
-  y: { duration: 15, repeat: Infinity, repeatType: "loop", ease: "linear" },
-};
-
-// const transition = {
-//   rotate: { duration: 4, repeat: Infinity, ease: "linear" },
-//   vertical: { duration: 4, repeat: Infinity, ease: "easeInOut" },
-//   horizontal: { duration: 4, repeat: Infinity, ease: "easeInOut" },
-// };
-
-export const AnimatedBlob = () => {
-  const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
+export const AnimatedBlob: React.FC<AnimatedBlobType> = ({
+  radius,
+  duration,
+  borderRadius,
+  backgroundColor,
+  top,
+  left,
+  clockwise,
+}) => {
+  const [coordinates, setCoordinates] = useState<{ x: number; y: number }>({
+    x: 0,
+    y: 0,
+  });
 
   useEffect(() => {
-    const radius = 200; // Radius of the circle
-    const duration = 20 * 1000; // 20 seconds
+    //const radius = 200; // Radius of the circle
+    //const duration = 20 * 1000; // 20 seconds
     const startTime = new Date().getTime();
 
     const animate = () => {
       const currentTime = new Date().getTime();
       const timePassed = currentTime - startTime;
-      const angle = ((timePassed % duration) / duration) * 360;
+      const angleDirection = clockwise ? 1 : -1;
+      const angle =
+        ((angleDirection * (timePassed % (duration * 1000))) /
+          (duration * 1000)) *
+        360;
       const x = radius * Math.cos((angle * Math.PI) / 180);
       const y = radius * Math.sin((angle * Math.PI) / 180);
       setCoordinates({ x, y });
@@ -48,18 +45,15 @@ export const AnimatedBlob = () => {
     };
 
     animate();
-  }, []);
+  }, [radius, duration, clockwise]);
 
   return (
     <motion.div
       className=" w-96 h-96 absolute"
-      initial={{ x: "0%", y: "0%", position: "absolute", top: 0, left: 0 }}
-      //animate="around"
-      //variants={blobVariants}
-      //transition={transition}
+      initial={{ position: "absolute", top: top, left: left }}
       style={{
-        borderRadius: "28% 72% 79% 21% / 21% 40% 60% 79% ",
-        backgroundColor: "#B99D76",
+        borderRadius: borderRadius,
+        backgroundColor: backgroundColor,
         filter: "blur(125px)",
         x: coordinates.x,
         y: coordinates.y,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,42 @@ export default function Home() {
         <button className="font-bold py-2 px-4 rounded">Global Rankings</button>
       </nav>
       <div className="flex flex-col items-center justify-center min-h-screen relative w-full">
-        <AnimatedBlob />
+        <AnimatedBlob
+          radius={200}
+          duration={20}
+          top={0}
+          left={0}
+          borderRadius="28% 72% 79% 21% / 21% 40% 60% 79%"
+          backgroundColor="#B99D76"
+          clockwise={true}
+        />
+        <AnimatedBlob
+          radius={200}
+          duration={20}
+          top={0}
+          left={window.innerWidth - 384}
+          borderRadius="61% 39% 19% 81% / 72% 40% 60% 28%"
+          backgroundColor="#0080DA"
+          clockwise={false}
+        />
+        <AnimatedBlob
+          radius={200}
+          duration={20}
+          top={window.innerHeight - 384}
+          left={0}
+          borderRadius="61% 39% 19% 81% / 29% 88% 12% 71%"
+          backgroundColor="#00218A"
+          clockwise={false}
+        />
+        <AnimatedBlob
+          radius={200}
+          duration={20}
+          top={window.innerHeight - 384}
+          left={window.innerWidth - 384}
+          borderRadius="29% 71% 19% 81% / 47% 20% 80% 53%"
+          backgroundColor="#7391AF"
+          clockwise={true}
+        />
         <div className="">
           <h1 className=" text-8xl font-bold mb-4 text-center">
             Global Power Ranking
@@ -19,28 +54,7 @@ export default function Home() {
             Predictions for LoL esports teams
           </h2>
         </div>
-        {/* <div className="flex justify-center items-center">
-          <div
-            className="w-48 h-48 rounded-full -ml-10 blur-xl"
-            style={{
-              borderRadius: "61% 39% 19% 81% / 72% 40% 60% 28% ",
-              backgroundColor: "#0080DA",
-            }}
-          ></div>
-          <div
-            className="w-48 h-48 rounded-full -ml-10 blur-xl"
-            style={{
-              borderRadius: "61% 39% 19% 81% / 29% 88% 12% 71% ",
-              backgroundColor: "#00218A",
-            }}
-          ></div>
-          <div
-            className="w-48 h-48 rounded-full -ml-10 blur-xl"
-            style={{
-              borderRadius: "29% 71% 19% 81% / 47% 20% 80% 53% ",
-              backgroundColor: "#7391AF",
-            }}
-          ></div>
+        {/* <div className="flex justify-center items-center">      
         </div> */}
       </div>
     </div>
